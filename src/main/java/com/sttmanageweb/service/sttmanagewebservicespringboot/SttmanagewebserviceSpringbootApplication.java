8b3d8fe0b554faaf8d5c4e2ea674792371dba84e
minseok-sms <tjalstjr111@gmail.com>
2022-06-16 18:15:52 +0900

    project start

---
+++ src/main/java/com/sttmanageweb/service/sttmanagewebservicespringboot/SttmanagewebserviceSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.sttmanageweb.service.sttmanagewebservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttmanagewebserviceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SttmanagewebserviceSpringbootApplication.class, args);
+        System.out.println("ss");
+    }
+
+}
---
+++ src/test/java/com/sttmanageweb/service/sttmanagewebservicespringboot/SttmanagewebserviceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sttmanageweb.service.sttmanagewebservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttmanagewebserviceSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
